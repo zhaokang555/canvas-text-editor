b0e53e1b8970df5aa2e626721390ea889eaf7e1f
kangzhao <forzhaokang@gmail.com>
2022-01-18 11:35:58 +0800

    feat: calculate text bounding box

--- src/core/CanvasTextEditor.ts
+++ src/core/CanvasTextEditor.ts
@@ -5,15 +5,40 @@ export class CanvasTextEditor {
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    // requestAnimationFrame(this.render);
     this.render();
   }
 
-  render() {
-    requestAnimationFrame(() => {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.ctx.fillStyle = '#000';
-      this.ctx.font = '16px sans-serif';
-      this.ctx.fillText('Hello, world!', 0, 16);
-    });
-  }
+  render = () => {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = '#000';
+    this.ctx.font = '100px sans-serif';
+    this.ctx.textBaseline = 'top';
+    const text = 'abg';
+
+    const textMetrics = this.ctx.measureText(text);
+    console.log(textMetrics);
+    const {
+      actualBoundingBoxDescent,
+      fontBoundingBoxDescent,
+      width,
+    } = textMetrics;
+
+    const textPos = {x: 100, y: 100};
+    this.ctx.fillText(text, textPos.x, textPos.y);
+    this.ctx.strokeStyle = 'red';
+    this.ctx.strokeRect(
+      textPos.x,
+      textPos.y,
+      width,
+      actualBoundingBoxDescent
+    );
+    this.ctx.strokeStyle = 'green';
+    this.ctx.strokeRect(
+      textPos.x,
+      textPos.y,
+      width,
+      fontBoundingBoxDescent
+    );
+  };
 }
