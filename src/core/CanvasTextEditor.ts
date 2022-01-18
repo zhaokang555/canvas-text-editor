cefa8fe1f5ee9bc0d3f17a3933723bf449a41bf3
kangzhao <forzhaokang@gmail.com>
2022-01-18 13:55:48 +0800

    feat: change cursor style by mouse position

--- src/core/CanvasTextEditor.ts
+++ src/core/CanvasTextEditor.ts
@@ -1,44 +1,49 @@
+import { CanvasTextEditorText } from './CanvasTextEditorText';
+
 export class CanvasTextEditor {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
+  textList: CanvasTextEditorText[] = [];
+  mousePos: { x: number, y: number } = {x: 0, y: 0};
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-    // requestAnimationFrame(this.render);
-    this.render();
-  }
-
-  render = () => {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.textList = [
+      new CanvasTextEditorText('abc', 100, 100, this.ctx),
+      new CanvasTextEditorText('def', 100, 200, this.ctx),
+      new CanvasTextEditorText('ghi', 100, 300, this.ctx),
+    ];
     this.ctx.fillStyle = '#000';
     this.ctx.font = '100px sans-serif';
     this.ctx.textBaseline = 'top';
-    const text = 'abg';
+    requestAnimationFrame(this.render);
+    this.canvas.addEventListener('mousemove', this.handleMousemove);
+  }
 
-    const textMetrics = this.ctx.measureText(text);
-    console.log(textMetrics);
-    const {
-      actualBoundingBoxDescent,
-      fontBoundingBoxDescent,
-      width,
-    } = textMetrics;
+  render = (time: number) => {
+    requestAnimationFrame(this.render);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.textList.forEach((text) => text.render());
+    if (this.checkMouseIsHoverOnText()) {
+      this.canvas.style.cursor = 'text';
+    } else {
+      this.canvas.style.cursor = 'default';
+    }
+  };
 
-    const textPos = {x: 100, y: 100};
-    this.ctx.fillText(text, textPos.x, textPos.y);
-    this.ctx.strokeStyle = 'red';
-    this.ctx.strokeRect(
-      textPos.x,
-      textPos.y,
-      width,
-      actualBoundingBoxDescent
-    );
-    this.ctx.strokeStyle = 'green';
-    this.ctx.strokeRect(
-      textPos.x,
-      textPos.y,
-      width,
-      fontBoundingBoxDescent
-    );
+  handleMousemove = (evt: MouseEvent) => {
+    const rect = this.canvas.getBoundingClientRect();
+    this.mousePos.x = evt.clientX - rect.left;
+    this.mousePos.y = evt.clientY - rect.top;
+  };
+
+  checkMouseIsHoverOnText = () => {
+    return this.textList.some(text => {
+      return this.mousePos.x >= text.left
+        && this.mousePos.y >= text.top
+        && this.mousePos.x <= text.left + text.width
+        && this.mousePos.y <= text.top + text.height;
+    });
   };
 }
---
+++ src/core/CanvasTextEditorText.ts
@@ -0,0 +1,22 @@
+export class CanvasTextEditorText {
+  content: string;
+  left = 0;
+  top = 0;
+  width = 0;
+  height = 0;
+  ctx: CanvasRenderingContext2D;
+
+  constructor(content: string, left: number, top: number, ctx: CanvasRenderingContext2D) {
+    this.content = content;
+    this.left = left;
+    this.top = top;
+    this.ctx = ctx;
+    const textMetrics = ctx.measureText(content);
+    this.width = textMetrics.width;
+    this.height = textMetrics.fontBoundingBoxDescent;
+  }
+
+  render = () => {
+    this.ctx.fillText(this.content, this.left, this.top);
+  };
+}
