b0d5891c3b0506ed488f06380bb6f49de65fab12
kang.zhao <forzhaokang@gmail.com>
2022-03-09 11:39:14 +0800

    feat: Add HalfChar

--- src/core/CanvasTextEditorHalfChar.ts
+++ src/core/CanvasTextEditorHalfChar.ts
@@ -1,6 +1,14 @@
 import ClickableZone from './mouse/ClickableZone';
+import MousedownZone from './mouse/MousedownZone';
 
 export default class CanvasTextEditorHalfChar {
+  mousedownZone: MousedownZone;
+
   constructor(public clickableZone: ClickableZone) {
+    this.mousedownZone = new MousedownZone(/* todo */);
+  }
+
+  setPosition(left: number, top: number) {
+    // todo
   }
 }
